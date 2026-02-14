75d621cf6a9ed3f544898295a8820f26a83294e1
Stiven Laiton <69602896+slaiton@users.noreply.github.com>
2026-02-13 21:09:21 -0500

    Login usuario

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -18,7 +18,7 @@ interface AuthResponse {
  * @returns Objeto con token e IDs relacionados.
  */
 
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 export const authenticateUser = async (
   credentials: LoginParams,
   remember: boolean = false
