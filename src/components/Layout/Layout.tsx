a474020a1a0e7783bd26a123d9af8a7a43fe6593
Stiven Laiton <69602896+slaiton@users.noreply.github.com>
2026-02-13 20:34:27 -0500

    Visual de logi usuario

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { layoutStyles } from "./layout.styles";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { AuthContext } from "../../shared/context/AuthContext";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-      const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const { isAuthenticated } = useContext(AuthContext) || {};
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        sidebarOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        setSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [sidebarOpen]);
   return (
     <div style={layoutStyles.container}>
-      {/* <Sidebar open={sidebarOpen} /> */}
-      <Sidebar open={sidebarOpen}  onLinkClick={() => setSidebarOpen(false)}/>
+      {isAuthenticated && (      
+      <Sidebar open={sidebarOpen} onLinkClick={() => setSidebarOpen(false)} />
+      )}
+
       <div style={layoutStyles.main}>
         <Navbar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
         <div style={layoutStyles.content}>{children}</div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
